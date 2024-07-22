abc9f3d3dc4f83b511d916a637fbcabbb279972d
xinla <1570728529@qq.com>
2024-07-22 14:21:53 +0800

    logging.interceptor

--- src/common/interceptor/logging.interceptor.ts
+++ src/common/interceptor/logging.interceptor.ts
@@ -1,24 +1,30 @@
-import { Injectable, NestInterceptor, ExecutionContext, CallHandler, RequestTimeoutException } from '@nestjs/common';
-import { Observable, throwError, TimeoutError } from 'rxjs';
-import { catchError, map, tap, timeout } from 'rxjs/operators';
+import { Injectable, NestInterceptor, ExecutionContext, CallHandler, RequestTimeoutException } from '@nestjs/common'
+import { Observable, throwError, TimeoutError } from 'rxjs'
+import { catchError, map, tap, timeout } from 'rxjs/operators'
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    console.log('Before...');
+    let req = context.switchToHttp().getRequest()
+    console.log('请求数据：', req.url, req.method, { query: req.query, body: req.body, params: req.params })
 
     return next.handle().pipe(
       timeout(5000),
       catchError((err) => {
         if (err instanceof TimeoutError) {
-          return throwError(() => new RequestTimeoutException());
+          return throwError(() => new RequestTimeoutException())
         }
-        return throwError(() => err);
+        return throwError(() => err)
       }),
-      map((data) => ({
-        code: data?.code ?? 200,
-        data: data?.data || data,
-      })),
-    );
+      map((data) => {
+        // let res = context.switchToHttp().getResponse()
+        data = {
+          code: data?.code ?? 200,
+          data: data?.data || data,
+        }
+        console.log(req.url, '返回数据：', data)
+        return data
+      }),
+    )
   }
 }
