ad233d74cb83e424d08433dbfda3ec9d408ed521
xinla <1570728529@qq.com>
2024-10-12 03:20:57 +0800

    修改BaseController和SystenConfigsController
    在BaseController中，修改list方法的返回类型，允许返回T[]类型。
    在SystenConfigsController中，添加新的list方法，使用@Public装饰器使其公开可访问，并处理查询参数默认值。
    还添加了一个返回404状态码的删除方法del。

--- src/modules/configs/controller.ts
+++ src/modules/configs/controller.ts
@@ -1,13 +1,26 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Put, Query, Req } from '@nestjs/common'
+import { Controller, Get, Post, Body, Patch, Param, Delete, Put, Query, Req, HttpCode } from '@nestjs/common'
 import { SystenConfigsService } from './service'
 import { QueryListDto, ResponseListDto } from 'src/common/dto'
 import { UpdateResult } from 'typeorm'
 import { SystenConfig } from './entity'
 import { BaseController } from 'src/common/BaseController'
+import { Public } from '../auth/constants'
 
 @Controller('system/configs')
 export class SystenConfigsController extends BaseController<SystenConfig, SystenConfigsService> {
   constructor(readonly service: SystenConfigsService) {
     super(service)
   }
+
+  @Get('list')
+  @Public()
+  async list(@Query() query: QueryListDto) {
+    query.pageNum ??= 1
+    query.pageSize ??= 10
+    return this.service.list(query)
+  }
+
+  @HttpCode(404)
+  @Delete('del/:ids')
+  async del() {}
 }
