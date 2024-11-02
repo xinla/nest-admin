1068449e26ed4ba62eb29c81414c2c61f01bac06
xinla <1570728529@qq.com>
2024-11-03 02:01:23 +0800

    优化配置文件

--- config/index.ts
+++ config/index.ts
@@ -1,8 +1,39 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm'
 import dayjs from 'dayjs'
-// import { BaseSubscriber } from 'src/common/entity/base'
-// import { secret } from './secret'
 import { accessSync, constants } from 'fs'
+import merge from 'lodash/merge'
+const mode = process.argv.find((e) => e.includes('env=')).split('=')[1]
+
+const env = {
+  dev: {
+    database: {
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'a1111111',
+      database: 'nest_admin',
+      // entities: [],
+      synchronize: true,
+      // dateStrings: true, // datetime无效，timestamp有效
+      autoLoadEntities: true,
+      // subscribers: [BaseSubscriber],
+      logging: true,
+    },
+  },
+  prod: {
+    database: {
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'your password',
+      database: 'nest_admin',
+      synchronize: true,
+      autoLoadEntities: true,
+    },
+  },
+}
 
 export const config = {
   adminKey: 'admin',
@@ -12,46 +43,18 @@ export const config = {
   }, // 到当天结束过期
   // jwtExpires: '1d',
   jwtSecret: 'DO NOT USE THIS VALUE. INSTEAD, CREATE A COMPLEX SECRET AND KEEP IT SAFE OUTSIDE OF THE SOURCE CODE.',
+
+  ...env[mode],
 }
 
-export const databaseList: { dev: TypeOrmModuleOptions; prod } = {
-  dev: {
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: 'a1111111',
-    database: 'nest_admin',
-    // entities: [],
-    synchronize: true,
-    // dateStrings: true, // datetime无效，timestamp有效
-    autoLoadEntities: true,
-    // subscribers: [BaseSubscriber],
-    logging: true,
-  },
-  prod: {
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: 'your password',
-    database: 'nest_admin',
-    synchronize: true,
-    autoLoadEntities: true,
-  },
+try {
+  // accessSync('config/secret.ts', constants.F_OK)
+  const { secret } = require(`./${'secret'}.js`)
+
+  merge(config, secret, secret[mode])
+} catch (err) {
+  // console.error('no access!')
 }
-
-const mode = process.argv.find((e) => e.includes('env=')).split('=')[1]
-
 export const database = async () => {
-  if (mode == 'prod') {
-    try {
-      // accessSync('config/secret.ts', constants.F_OK)
-      const { secret } = await import(`./${'secret'}.js`)
-      databaseList[mode].password = secret.mysqlPassword
-    } catch (err) {
-      // console.error('no access!')
-    }
-  }
-  return databaseList[mode]
+  return config.database
 }
